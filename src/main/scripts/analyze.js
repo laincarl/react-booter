6e7b3054ba542eb87298e0918041b219a7831037
laincarl <1047972853@qq.com>
2018-12-04 10:16:37 +0800

    [FIX]修复analyze名令找不到

--- src/main/scripts/analyze.js
+++ src/main/scripts/analyze.js
@@ -2,7 +2,6 @@ import webpack from 'webpack';
 import initMain from '../utils/initMain';
 import getAnalyzeConfig from '../webpack/webpack.analyze';
 
-console.log('ana');
 initMain().then(() => {
   webpack(getAnalyzeConfig(), (err, stats) => {
     if (err !== null) {
